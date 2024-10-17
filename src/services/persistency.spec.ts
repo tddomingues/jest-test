cb42e2a9a28bc56d6c9dba72295e7a4e5ed52214
tiago domingues <ttx8000@gmail.com>
2024-10-17 13:05:34 -0300

    primeiros testes

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,25 +1,40 @@
-import { Persistency } from './persistency';
+// import { Persistency } from './persistency';
 
-describe('Persistency', () => {
-  afterEach(() => jest.clearAllMocks());
+// describe('Persistency', () => {
+//   afterEach(() => jest.clearAllMocks());
 
-  it('should return undefined', () => {
-    // System under test
-    const sut = new Persistency();
-    expect(sut.saveOrder()).toBeUndefined();
+//   it('should return undefined', () => {
+//     // System under test
+//     const sut = new Persistency();
+//     expect(sut.saveOrder()).toBeUndefined();
+//   });
+
+//   it('should call console.log once', () => {
+//     const sut = new Persistency();
+//     const consoleSpy = jest.spyOn(console, 'log');
+//     sut.saveOrder();
+//     expect(consoleSpy).toHaveBeenCalledTimes(1);
+//   });
+
+//   it('should call console.log with "Pedido salvo com sucesso..."', () => {
+//     const sut = new Persistency();
+//     const consoleSpy = jest.spyOn(console, 'log');
+//     sut.saveOrder();
+//     expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
+//   });
+// });
+
+describe('TESTANDO ALGUMA COISA', () => {
+  it('Isso deveria retornar 1', () => {
+    const number = 1;
+    expect(number).toBe(1); //espero que a variável number seja 1
+    //expect(number).not.toBe(1); //espero que a variável number nao seja 1
   });
+});
 
-  it('should call console.log once', () => {
-    const sut = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    sut.saveOrder();
-    expect(consoleSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should call console.log with "Pedido salvo com sucesso..."', () => {
-    const sut = new Persistency();
-    const consoleSpy = jest.spyOn(console, 'log');
-    sut.saveOrder();
-    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
+describe('TESTANDO OUTRA COISA', () => {
+  test('Isoo deveria retornar Tiago', () => {
+    const nome = 'Tiago';
+    expect(nome).toBe('Tiago');
   });
 });
